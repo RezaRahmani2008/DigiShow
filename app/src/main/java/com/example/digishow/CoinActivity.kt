4cd1006444223f072e72ef04af38da6a65afa253
Rio <Rioz2008x@gmail0com>
2023-10-16 08:26:38 +0330

    finish activity_coin.xml

---
+++ app/src/main/java/com/example/digishow/CoinActivity.kt
@@ -0,0 +1,15 @@
+package com.example.digishow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.digishow.databinding.ActivityCoinBinding
+
+class CoinActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCoinBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCoinBinding.inflate(layoutInflater)
+        setContentView( binding.root )
+    }
+}
